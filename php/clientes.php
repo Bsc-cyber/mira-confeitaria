8a985c70c5f328f5c53aea394698cafad7af1421
Bsc-cyber <912932@senacminas.edu.br>
2026-07-02 21:03:10 -0300

    02

--- php/clientes.php
+++ php/clientes.php
@@ -53,7 +53,6 @@ $clientes = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MIRA Confeitaria - Cadastro de Clientes</title>
     <link rel="stylesheet" href="../css/clientes.css?v=3.0">
-    <link rel="stylesheet" href="../css/responsivo.css">
 </head>
 <body>
 
